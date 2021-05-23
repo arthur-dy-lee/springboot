8438aa790c7a3ff5cde8be59f83b24b5d7252c43
arthur-dy-lee <arthur.dy.lee@gmail.com>
2021-05-23 16:43:08 +0800

    redis 配置，并注掉

--- springboot/src/main/java/arthur/dy/lee/SpringbootApplication.java
+++ springboot/src/main/java/arthur/dy/lee/SpringbootApplication.java
@@ -3,8 +3,10 @@ package arthur.dy.lee;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 @MapperScan(value = {"arthur.dy.lee.dao"})
 public class SpringbootApplication {
 
@@ -12,5 +14,17 @@ public class SpringbootApplication {
         SpringApplication.run(SpringbootApplication.class, args);
     }
 
+//    @Bean
+//    ConfigureRedisAction configureRedisAction() {
+//        return ConfigureRedisAction.NO_OP;
+//    }
+//
+//
+//    @Bean
+//    public JedisConnectionFactory redisConnectionFactory() {
+//        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("127.0.0.1", 6379);
+//        config.setPassword("123456a?");
+//        return new JedisConnectionFactory(config);
+//    }
 
 }
