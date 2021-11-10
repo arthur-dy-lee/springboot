2b139844e82cee427c1a5234cfac5cacd6c955cb
arthur-dy-lee <arthur.dy.lee@gmail.com>
2021-11-10 23:17:42 +0800

    最长公共前缀

---
+++ springboot/src/main/java/arthur/dy/lee/alg/str/LongestCommonPrefix.java
@@ -0,0 +1,10 @@
+package arthur.dy.lee.alg.str;
+
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+
+        StringBuilder result = new StringBuilder();
+
+        return result.toString();
+    }
+}
