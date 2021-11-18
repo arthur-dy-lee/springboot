da370a3495b7e9121e76d25c7dc4d53f350ee4e9
arthur-dy-lee <arthur.dy.lee@gmail.com>
2021-11-18 08:12:11 +0800

    括号生成

---
+++ springboot/src/main/java/arthur/dy/lee/alg/str/GenerateParenthesis.java
@@ -0,0 +1,80 @@
+package arthur.dy.lee.alg.str;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 22. 括号生成
+ * https://leetcode-cn.com/problems/generate-parentheses/solution/hui-su-suan-fa-by-liweiwei1419/
+ * <p>
+ * 数字 n代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ * <p>
+ * 有效括号组合需满足：左括号必须以正确的顺序闭合。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 3
+ * 输出：["((()))","(()())","(())()","()(())","()()()"]
+ * 示例 2：
+ * <p>
+ * 输入：n = 1
+ * 输出：["()"]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/generate-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class GenerateParenthesis {
+
+    // 做减法
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        // 特判
+        if (n == 0) {
+            return res;
+        }
+
+        // 执行深度优先遍历，搜索可能的结果
+        dfs("", 0, 0, n, res);
+        return res;
+    }
+
+    /**
+     * @param curStr 当前递归得到的结果
+     * @param left   左括号还有几个可以使用
+     * @param right  右括号还有几个可以使用
+     * @param res    结果集
+     */
+    private void dfs(String curStr, int left, int right, int n, List<String> res) {
+        // 因为每一次尝试，都使用新的字符串变量，所以无需回溯
+        // 在递归终止的时候，直接把它添加到结果集即可，注意与「力扣」第 46 题、第 39 题区分
+        if (left == n && right == n) {
+            res.add(curStr);
+            return;
+        }
+
+        // 剪枝（如图，左括号可以使用的个数严格大于右括号可以使用的个数，才剪枝，注意这个细节）
+        if (left < right) {
+            return;
+        }
+
+        if (left < n) {
+            dfs(curStr + "(", left + 1, right, n, res);
+        }
+
+        if (right < n) {
+            dfs(curStr + ")", left, right + 1, n, res);
+        }
+    }
+
+    public static void main(String[] args) {
+        GenerateParenthesis gp = new GenerateParenthesis();
+        System.out.println(JSONObject.toJSONString(gp.generateParenthesis(2)));
+        System.out.println(JSONObject.toJSONString(gp.generateParenthesis(3)));
+    }
+
+}
