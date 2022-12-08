367ca1e34557bd1524e67805348e3a3fd6319f22
arthur-dy-lee <arthur.dy.lee@gmail.com>
2022-12-08 08:25:10 +0800

    leetcode: listNode

---
+++ springboot/src/main/java/arthur/dy/lee/alg2/_3_lengthOfLongestSubstring.java
@@ -0,0 +1,17 @@
+package arthur.dy.lee.alg2;
+
+public class _3_lengthOfLongestSubstring {
+
+    public static int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
